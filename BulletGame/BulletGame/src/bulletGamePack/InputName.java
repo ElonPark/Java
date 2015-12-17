f4f2ad683fd8877504ba439d7125b1e53059b94e
NebulaPark <tjddns1222@gmail.com>
2015-12-17 13:42:25 +0900

    자바 기말 과제

---
+++ BulletGame/BulletGame/src/bulletGamePack/InputName.java
@@ -0,0 +1,55 @@
+package bulletGamePack;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class InputName extends JFrame implements ActionListener {
+	
+	//게임을 시작할때 창을 뜨워 유저의 이름을 받아온다. 
+	JLabel jLal = new JLabel("이름을 입력하세요 ");
+	JTextField txtF = new JTextField(15);
+	String name;
+    
+	JButton btnExit = new JButton("닫기");
+
+	JPanel p1 = new JPanel();
+	
+	void Input() {
+		txtF.setText("용사 AAA");
+		p1.add(jLal);
+		p1.add(txtF);
+
+		p1.add(btnExit);
+
+		add("Center", p1);
+
+		btnExit.addActionListener(this);
+
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(200, 120);
+		setVisible(true);
+		
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == btnExit) { 
+			//닫기 버튼을 누르면 라벨에 유저이름을 입력하고 게임을 구성한다.
+			BulletGame bg = new BulletGame();
+			bg.LUser.setText("  유저: "+txtF.getText());
+			bg.gameFrame();
+			this.dispose();
+		}
+	
+	}
+}
---
+++ BulletGame/ThreadTimerEx/src/ThreadTimerEx.java
@@ -0,0 +1,54 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+class TimerThread extends Thread {
+	JLabel timerLabel;
+
+	public TimerThread(JLabel timerLabel) {
+		this.timerLabel = timerLabel;
+	}
+
+	// 스레드 코드. run()이 종료하면 스레드 종료
+	public void run() {
+		int n = 0;
+		while (true) {
+			timerLabel.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(500);
+			}catch(Exception e){
+				return;
+			}
+		}
+	}
+}
+
+public class ThreadTimerEx extends JFrame {
+	public ThreadTimerEx() {
+		setTitle("Thread를 상속받은 타이머 스레드 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		//타이머 값을 출력한 레이블 생성
+		JLabel timerLabel = new JLabel();
+		timerLabel.setFont(new Font("Gothic", Font.ITALIC, 80));
+		c.add(timerLabel);
+		
+		TimerThread th = new TimerThread(timerLabel);
+		
+		setSize(250, 150);
+		setVisible(true);
+		
+		th.start();
+	}
+
+	public static void main(String[] args) {
+		new ThreadTimerEx();
+	}
+
+}
